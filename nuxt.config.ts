9ef0a93a38c250848e6f0e1dbefb570ab15f4874
Andrei <andrei.hudalla@outlook.com>
2023-12-24 20:46:01 +0300

    Added security features

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', 'nuxt-icon', '@nuxtjs/supabase'],
-  app: {
-	  pageTransition: { name: 'page', mode: 'out-in' },
-	  layoutTransition: { name: 'page', mode: 'out-in' },
-	  head: {
-		htmlAttrs: {
-		  lang: 'ru',
+	devtools: { enabled: true },
+	modules: ['@nuxt/ui', 'nuxt-icon', '@nuxtjs/supabase', 'nuxt-security'],
+	app: {
+		pageTransition: { name: 'page', mode: 'out-in' },
+		layoutTransition: { name: 'page', mode: 'out-in' },
+		head: {
+			htmlAttrs: {
+				lang: 'ru',
+			},
 		},
-	  },
-  },
-  css: [
-	  "assets/global.scss"
-  ],
-  supabase: {
-	  redirect: false
-  }
+	},
+	css: [
+		"assets/global.scss"
+	],
+	security: {
+		headers: {
+			crossOriginEmbedderPolicy: false,
+			contentSecurityPolicy: {
+				"font-src": false
+			}
+		}
+	},
+	supabase: {
+		redirect: false
+	}
 })
--- server/api/submit_vote.post.ts
+++ server/api/submit_vote.post.ts
@@ -1,15 +1,23 @@
 import { serverSupabaseServiceRole } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
-	const supabase = serverSupabaseServiceRole(event);
-	const body = await readBody(event)
-	const { UID, vote } = body
-	const { error } = await supabase.from("votes").insert({
-		voter_id: UID,
-		vote: vote
-	} as any)
-	if (error) throw createError({
-		statusCode: 400,
-		statusMessage: error.code
-	})
+	if (process.server) {
+		const req = event.node.req
+		const headers = (req && req.headers) ? Object.assign({}, req.headers) : {};
+		const xForwardedFor = headers['x-forwarded-for'];
+		const xRealIp = headers['x-real-ip'];
+		const IP = xForwardedFor || xRealIp || event.context.clientAddress
+		const supabase = serverSupabaseServiceRole(event);
+		const body = await readBody(event)
+		const { UID, vote } = body
+		const { error } = await supabase.from("votes").insert({
+			voter_id: UID,
+			vote: vote,
+			ip: IP
+		} as any)
+		if (error) throw createError({
+			statusCode: 400,
+			statusMessage: error.code
+		})
+	}
 })

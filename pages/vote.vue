0d0bc87f25ad3b4e24f1e4e58621591957019f4e
Andrei <andrei.hudalla@outlook.com>
2023-12-24 16:53:34 +0300

    Create vote.vue

---
+++ pages/vote.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div class="__only-vote">
+        <Voting />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.__only-vote {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+}
+</style>

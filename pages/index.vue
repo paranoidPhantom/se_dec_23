93460c6f5105cbc47cf435bc0b09b2c5f166e693
Andrei <andrei.hudalla@outlook.com>
2023-12-24 10:44:10 +0300

    Added placeholder background (label)

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@ definePageMeta({
 </script>
 
 <template>
+	<p class="notice" style="position: fixed; z-index: -1; left: 1rem; top: 1rem;">(Ждём фон от дизайнера)</p>
     <div class="__home px-6 sm:px-12 md:px-20 lg:px-32 pt-12">
 		<Voting />
 	</div>

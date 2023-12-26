b25b941e6b96eee5e9bad669fbf85a70f8afe9e7
Andrei <andrei.hudalla@outlook.com>
2023-12-26 22:20:00 +0300

    Added forced dark mode and fixed snow

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const { website_name } = useAppConfig();
 
+const router = useRouter();
 const route = useRoute();
 
 watchEffect(() => {
@@ -18,17 +19,45 @@ watchEffect(() => {
                 href: "/favicon.svg",
             },
         ],
-		script: [
-			{
-				src: "/snow/script.js",
-			}
-		]
+        script: [
+            {
+                src: "/snow/script.js",
+            },
+        ],
     });
 });
+
+onMounted(() => {
+    let countdown = 10;
+    const themeCheckInterval = setInterval(() => {
+        countdown--;
+        if (countdown > 0) {
+            if (document.documentElement.classList.contains("dark"))
+                clearInterval(themeCheckInterval);
+            else countdown--;
+        } else {
+            document.documentElement.classList.add("dark");
+            clearInterval(themeCheckInterval);
+        }
+    }, 100);
+});
+
+defineShortcuts({
+	shift_a: () => router.push("/admin") 
+})
+
+const snow = ref(true)
+watchEffect(() => {
+	snow.value = route.meta.layout !== 'admin'
+})
 </script>
 
 <template>
-    <div id="snow" v-show="route.meta.layout === 'default'" data-count="30"></div>
+    <div
+        id="snow"
+        v-show="snow"
+        data-count="30"
+    ></div>
     <NuxtLayout>
         <NuxtPage />
     </NuxtLayout>
